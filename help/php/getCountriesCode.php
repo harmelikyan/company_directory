0acb4e46bb3c18322ad193503cea729e877a63f7
harmelikyan <65981737+harmelikyan@users.noreply.github.com>
2021-08-19 13:32:18 +0300

    Update getCountriesCode.php

--- help/php/getCountriesCode.php
+++ help/php/getCountriesCode.php
@@ -1,19 +1,16 @@
 <?php
-
+ 
     $url = file_get_contents("countryBorders.geo.json");
     $json = json_decode($url);
     $features = $json->features;
-
-    $countries = array();
-    for($i = 0; $i < sizeof($features); $i++) {
-        $feature = $features[$i];
-        $country_name = $feature->properties->name;
-        $country_iso = $feature->properties->iso_a2;
-        $array = [$country_name, $country_iso];
-        array_push($countries, $array);
+ 
+    $countries = [];
+    for($i = 0; $i < count($features); $i++) {
+        
+        array_push($countries, $features[$i]);
     }
-
-    print_r(json_encode($countries));
-
+ 
+    echo json_encode($countries);
+ 
 
 ?>
