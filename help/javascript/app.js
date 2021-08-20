c236b52555e48d4063a904620420a8b061b447ef
harmelikyan <65981737+harmelikyan@users.noreply.github.com>
2021-08-20 13:22:00 +0300

    Update app.js

--- help/javascript/app.js
+++ help/javascript/app.js
@@ -1,4 +1,3 @@
-//init maps
 const map = L.map('issMap').fitWorld();
 map.locate({setView: true, maxZoom: 16});
 
@@ -7,9 +6,8 @@ const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 const tiles = L.tileLayer(tileUrl, { attribution })
 tiles.addTo(map)
 
+//shows latlng on click
 map.on('click', onMapClick);
-
-
 var popup = L.popup();
 function onMapClick(e) {
     popup
@@ -18,9 +16,9 @@ function onMapClick(e) {
         .openOn(map);
 }
 
+
 map.on('click', onMapClick);
-
-
+var countryBoundary = new L.geoJson().addTo(map);
 
 function onLocationFound(e) {
     var radius = e.accuracy;
@@ -41,30 +39,78 @@ map.on('locationerror', onLocationError);
 
 
 
-//getting country names
-  function get_country() {
+//   countryName
+function getCountry() {
     $.ajax({
       url: 'php/getCountriesCode.php',
       type: 'GET',
       dataType: 'json',
       
-      success: function(json) {
-        // var countries = JSON.parse(json);
-        var countries = JSON.stringify(json);
-    //    console.log(countries)
-
-        var output = "";
-        for(var country of countries) {
-            output +=
-            '<option value="' + country[1] + '">' + country[0] + "</option>";
+      success: function(result) {
+        console.log(result)
+ 
+        for(var country of result) {
+         var countNames =  $('#countries').append(`<option value="${country["iso_a2"]}">${country["name"]}</option>`);
         }
-        $('#countries').html(output);
       },
       error: function(jqXHR, textStatus, errorThrown) {
-				// your error code
-			}
+        // your error code
+        console.log(jqXHR);
+      }
             
     })
     
   }
-  get_country();
+  getCountry();
+
+  //country border
+  function getCountryBorder(countryCode) {
+    $.ajax({
+      url: 'php/getCountryBorders.php',
+      type: 'GET',
+      dataType: 'json', 
+      data: {
+          countryCode: countryCode
+      },
+      
+      success: function(result) {
+      //  console.log (JSON.stringify(json))
+        countryBoundary.clearLayers();
+        countryBoundary.addData(result).setStyle(countryStyle());
+        const bounds = countryBoundary.getBounds();
+        map.fitBounds(bounds);
+ },
+
+      error: function(jqXHR, textStatus, errorThrown) {
+        // your error code
+        console.log(jqXHR);
+      }
+            
+    })
+    
+  }
+  getCountryBorder();
+
+
+  
+
+function countryStyle() {
+  return {
+    fillColor: "blue",
+    weight: 2,
+    fillOpacity: 0.6,
+
+  }
+}
+
+
+
+var latlang = [[[23.75975, -77.53466],[23.71, -77.78],[24.28615, -78.03405],[24.57564, -78.40848],[25.2103, -78.19087],[25.17, -77.89],[24.34, -77.54],[23.75975, -77.53466]]];
+// Creating poly line options
+var polyLineOptions = {color:'red'};
+// Creating multi poly-lines
+var polyline = L.polyline(latlang , polyLineOptions);
+// Adding multi poly-line to map
+map.fitBounds(polyline.getBounds());
+polyline.addTo(map);
+
