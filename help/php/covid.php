5b6ceb08d7decd7a093e3d4eefd6ce2a2a25ec98
harmelikyan <65981737+harmelikyan@users.noreply.github.com>
2021-09-19 23:40:49 +0400

    Update covid.php

--- help/php/covid.php
+++ help/php/covid.php
@@ -1,46 +1,11 @@
 <?php
-
-//error handler
-ini_set('display_errors', 'On');
-error_reporting(E_ALL);
-
-//runtime
-$executionStartTime = microtime(true);
-
+$lat = $_GET['lat'];
+$long = $_GET['lng'];
 $country_name = $_GET['country_code'];
-//api path
-$url= $data = file_get_contents('https://disease.sh/v3/covid-19/countries/$country_name?yesterday&strict&query');
-
-
-//curl initialize
-$ch = curl_init();
-//curl constants
-//verify the peer's SSL certificate
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-//Set an option for a cURL transfer
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-//provide the URL to use in the request
-curl_setopt($ch, CURLOPT_URL, $url);
-//curl execution
-$result = curl_exec($ch);
-//Close a cURL session
-curl_close($ch);
-
-$decode = json_decode($result, true);
-
-
-
-
-    $output['status']['code'] = "200";
-	$output['status']['name'] = "ok";
-	$output['status']['description'] = "success";
-	$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-	$output['data'] = $decode[''];
-
-    header('Content-Type: application/json; charset=UTF-8');
-
-
-
-echo json_encode($output);
-
+$data = file_get_contents('https://disease.sh/v3/covid-19/countries/$country_name?yesterday&strict&query');
+print_r($data);
 ?>
+
+
+
+
