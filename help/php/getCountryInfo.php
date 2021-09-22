e1fb793f1ca57e85869a1656c47f04102eb0f191
harmelikyan <65981737+harmelikyan@users.noreply.github.com>
2021-09-22 16:20:03 +0400

    Update getCountryInfo.php

--- help/php/getCountryInfo.php
+++ help/php/getCountryInfo.php
@@ -1,5 +1,41 @@
 <?php
-$country_code = $_GET['country_code'];
-$data = file_get_contents("https://restcountries.eu/rest/v2/alpha/$country_code");
-print_r($data);
+//error handler
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+//runtime
+$executionStartTime = microtime(true);
+//api path
+$url= 'https://restcountries.eu/rest/v2/alpha/' . REQUEST['country_code'];
+
+
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+//Set an option for a cURL transfer
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//provide the URL to use in the request
+curl_setopt($ch, CURLOPT_URL, $url);
+//curl execution
+$result = curl_exec($ch);
+//Close a cURL session
+curl_close($ch);
+
+$decode = json_decode($result, true);
+
+
+
+
+    $output['status']['code'] = "200";
+	$output['status']['name'] = "ok";
+	$output['status']['description'] = "success";
+	$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
+	$output['data'] = $decode;
+
+    header('Content-Type: application/json; charset=UTF-8');
+
+
+
+echo json_encode($output);
+
 ?>
