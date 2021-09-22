1b5332d35593fb639b4f90fa11dec4db25b38d5a
harmelikyan <65981737+harmelikyan@users.noreply.github.com>
2021-09-22 23:29:26 +0400

    Update getNews.php

--- help/php/getNews.php
+++ help/php/getNews.php
@@ -1,11 +1,47 @@
-
 <?php
+//error handler
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
 
-
-$lat = $_GET['lat'];
-$lng = $_GET['lng'];
+//runtime
+$executionStartTime = microtime(true);
+//api path
+// $lat = $_GET['lat'];
+// $lng = $_GET['lng'];
 $countryName = $_GET['countryCodeGlobal'];
 $current_date = date("Y-m-d");
-$data = file_get_contents('http://newsapi.org/v2/everything?q=' . $countryName . '&from=' . $current_date . '&sortBy=relevancy&apiKey=c196d2e348644b668ecfe80c1af6e7a8');
-print_r($data);
+$url= 'http://newsapi.org/v2/everything?q=' . $countryName . '&from=' . $current_date . '&sortBy=relevancy&apiKey=c196d2e348644b668ecfe80c1af6e7a8';
+
+
+//curl initialize
+$ch = curl_init();
+//curl constants
+//verify the peer's SSL certificate
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+//Set an option for a cURL transfer
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//provide the URL to use in the request
+curl_setopt($ch, CURLOPT_URL, $url);
+//curl execution
+$result = curl_exec($ch);
+//Close a cURL session
+curl_close($ch);
+
+$decode = json_decode($result, true);
+
+
+
+
+    $output['status']['code'] = "200";
+	$output['status']['name'] = "ok";
+	$output['status']['description'] = "success";
+	$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
+	$output['data'] = $decode['articles'];
+
+    header('Content-Type: application/json; charset=UTF-8');
+
+
+
+echo json_encode($output);
+
 ?>
