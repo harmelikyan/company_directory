c20d6d15e7e5078086131f8de2f5908d5d698512
harmelikyan <65981737+harmelikyan@users.noreply.github.com>
2021-09-29 19:09:54 +0400

    Delete help directory

--- help/php/getWeather.php
+++
@@ -1,5 +0,0 @@
-<?php
-$lat = $_GET['lat'];
-$lng = $_GET['lng'];
-$data = file_get_contents("https://api.openweathermap.org/data/2.5/onecall?units=metric&lat=$lat&lon=$lng&exclude=current,minutely,hourly,alerts&APPID=04504a1938ffdb3d3aac4fcbd978f355");
-print_r($data);
