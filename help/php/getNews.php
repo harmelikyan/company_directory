905bcfb23856cccea69af4d8a5689d6cf919849c
harmelikyan <65981737+harmelikyan@users.noreply.github.com>
2021-09-29 20:20:01 +0400

    Update getNews.php

--- help/php/getNews.php
+++ help/php/getNews.php
@@ -6,9 +6,6 @@ error_reporting(E_ALL);
 //runtime
 $executionStartTime = microtime(true);
 //api path
-// $lat = $_GET['lat'];
-// $lng = $_GET['lng'];
-// $countryName = $_GET['countryCodeGlobal'];
 $current_date = date("Y-m-d");
 $url= 'http://newsapi.org/v2/everything?q=' . $_REQUEST['countryCodeGlobal'] . '&from=' . $current_date . '&sortBy=relevancy&apiKey=c196d2e348644b668ecfe80c1af6e7a8';
 
@@ -45,23 +42,3 @@ $decode = json_decode($result, true);
 echo json_encode($output);
 
 ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $lat = $_GET['lat'];
-// $lng = $_GET['lng'];
-// $countryName = $_GET['countryCodeGlobal'];
-// $current_date = date("Y-m-d");
-// $data = file_get_contents('http://newsapi.org/v2/everything?q=' . $countryName . '&from=' . $current_date . '&sortBy=relevancy&apiKey=c196d2e348644b668ecfe80c1af6e7a8');
-// print_r($data);
-// ?> 
